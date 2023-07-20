afefc22beac818d4b799685ba062e8869b2f5a9f
hung <bahu@gmail.com>
2023-07-20 23:02:46 +0700

    update

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -32,14 +32,14 @@ config_integration.trace_integrations(["requests"])
 # Logging
 logger = logging.getLogger(__name__)
 handler = AzureLogHandler(
-    connection_string="InstrumentationKey=253aafe7-9c9d-425f-8bd2-475463ee08c6"
+    connection_string="InstrumentationKey=7736ba3e-4d2b-4c8e-817f-e9e5f81ed382"
 )
 handler.setFormatter(logging.Formatter("%(traceId)s %(spanId)s %(message)s"))
 logger.addHandler(handler)
 # Logging custom Events
 logger.addHandler(
     AzureEventHandler(
-        connection_string="InstrumentationKey=253aafe7-9c9d-425f-8bd2-475463ee08c6"
+        connection_string="InstrumentationKey=7736ba3e-4d2b-4c8e-817f-e9e5f81ed382"
     )
 )
 # Set the logging level
@@ -48,14 +48,14 @@ logger.setLevel(logging.INFO)
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
-    connection_string="InstrumentationKey=253aafe7-9c9d-425f-8bd2-475463ee08c6",
+    connection_string="InstrumentationKey=7736ba3e-4d2b-4c8e-817f-e9e5f81ed382",
 )
 view_manager.register_exporter(exporter)
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string="InstrumentationKey=253aafe7-9c9d-425f-8bd2-475463ee08c6"
+        connection_string="InstrumentationKey=7736ba3e-4d2b-4c8e-817f-e9e5f81ed382"
     ),
     sampler=ProbabilitySampler(1.0),
 )
@@ -66,7 +66,7 @@ app = Flask(__name__)
 middleware = FlaskMiddleware(
     app,
     exporter=AzureExporter(
-        connection_string="InstrumentationKey=253aafe7-9c9d-425f-8bd2-475463ee08c6"
+        connection_string="InstrumentationKey=7736ba3e-4d2b-4c8e-817f-e9e5f81ed382"
     ),
     sampler=ProbabilitySampler(rate=1.0),
 )
